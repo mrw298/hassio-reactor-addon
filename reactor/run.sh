0400c372486bc6056063acbcf417b98b82e23d88
mrw298 <3275620+mrw298@users.noreply.github.com>
2023-01-23 12:55:04 +0000

    Add more config options

--- reactor/run.sh
+++ reactor/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-# Blank for now
+CONFIG_DIR=/config/reactor
+CONFIG_SRC=/var/reactor/config
+
+# Map the config
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo "Migrating configration to Home assistant"
+  mv $CONFIG_SRC $CONFIG_DIR
+fi
+
+# Remove the directory inside the container and mount the Home assistant version instead
+rm -rf $CONFIG_SRC
+ln -s /config/reactor /var/reactor/config
+
+# Run the app
+node app.js
