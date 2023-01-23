3163266b9374f75026f8a5521c4f211d9b114ae9
mrw298 <3275620+mrw298@users.noreply.github.com>
2023-01-23 13:41:43 +0000

    get ingress working

--- reactor/run.sh
+++ reactor/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 CONFIG_DIR="/config/reactor"
-CONFIG_SRC="/var/reactor/config"
+CONFIG_SRC="/opt/reactor/dist-config"
 
-# Map the config
+# Setup the config
 if [ ! -d "$CONFIG_DIR" ]; then
-  echo "Migrating configration to Home assistant"
-  mv $CONFIG_SRC $CONFIG_DIR
+  echo "New setup, copy dist-config files to /config/reactor"
+  cp -vaR $CONFIG_SRC $CONFIG_DIR
+else
+  echo "Using existing set-up"
 fi
 
-# Remove the directory inside the container and mount the Home assistant version instead
-rm -rf $CONFIG_SRC
-ln -s /config/reactor /var/reactor/config
-
 # Run the app
-node app.js
+node app.js -c /config/reactor
