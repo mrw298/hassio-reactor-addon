3cfe4414a7f65935d4ef165c178719bbeb222645
mrw298 <3275620+mrw298@users.noreply.github.com>
2023-01-26 08:19:25 +0000

    first-cut of making config easier

--- reactor/run.sh
+++ reactor/run.sh
@@ -7,9 +7,16 @@ CONFIG_SRC="/opt/reactor/dist-config"
 if [ ! -d "$CONFIG_DIR" ]; then
   echo "New setup, copy dist-config files to /config/reactor"
   cp -vaR $CONFIG_SRC $CONFIG_DIR
+  
+  echo "Configure hass"
+  yq eval -i '(.controllers[] | select(.id=="hass").config.source) |= "ws://homeassistant.local:8123"' $CONFIG_DIR/reactor.yaml
+  yq eval -i '(.controllers[] | select(.id=="hass").enabled) |= true' $CONFIG_DIR/reactor.yaml
 else
   echo "Using existing set-up"
 fi
+
+
+#yq eval -i "(.controllers[] | select(.id==\"hass\").config.access_token) |= \"$HASSIO_TOKEN\"" $CONFIG_DIR/reactor.yaml
 
 # Run the app
 node app.js -c /config/reactor
